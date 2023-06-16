98285c03bfe061c800d39b1902c04d8da18a56be
Maxence BOCQUEL <maxence.bocquel@gmail.com>
2023-06-16 18:03:38 +0200

    post merge Maxence

---
+++ build/HttpReq.d
@@ -0,0 +1,5 @@
+build/HttpReq.o: HttpReq.cpp HttpReq.hpp webserv.hpp
+
+HttpReq.hpp:
+
+webserv.hpp:
---
+++ build/Launcher.d
@@ -0,0 +1,18 @@
+build/Launcher.o: Launcher.cpp Launcher.hpp webserv.hpp Server.hpp \
+  Location.hpp Client.hpp HttpReq.hpp HttpRes.hpp ConfParser.hpp
+
+Launcher.hpp:
+
+webserv.hpp:
+
+Server.hpp:
+
+Location.hpp:
+
+Client.hpp:
+
+HttpReq.hpp:
+
+HttpRes.hpp:
+
+ConfParser.hpp:
