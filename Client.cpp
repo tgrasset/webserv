af3a0aeccf9f6c29f1f682f230a0e0351d65efdd
Joseph LANZA <jlanza@student.42.fr>
2023-09-08 22:05:17 +0200

    clean code a bit

--- Client.cpp
+++ Client.cpp
@@ -6,7 +6,7 @@
 /*   By: jlanza <jlanza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 19:09:25 by mbocquel          #+#    #+#             */
-/*   Updated: 2023/09/08 21:49:13 by jlanza           ###   ########.fr       */
+/*   Updated: 2023/09/08 22:04:40 by jlanza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -394,16 +394,14 @@ void	Client::sendResponseBodyCgi(void)
 	std::vector<char> cgi_buff_header;
 	int byte_sent_header = 0;
 	cgi_buff_header = _res->getCgiBuff_header();
-	int	size_chunk_header = cgi_buff_header.size();
-	if (size_chunk_header > 0)
+	if (cgi_buff_header.size() > 0)
 	{
-		std::ostringstream ss;
-		std::string str_size_chunk(ss.str());
+		std::string str_size_chunk("");
 		std::vector<char>	to_send(str_size_chunk.begin(), str_size_chunk.end());
 		to_send.insert(to_send.end(), cgi_buff_header.begin(), cgi_buff_header.end());
 		to_send.push_back('\r');
 		to_send.push_back('\n');
-		byte_sent_header = send(this->_com_socket, to_send.data(), to_send.size(), 0);
+		int byte_sent_header = send(this->_com_socket, to_send.data(), to_send.size(), 0);
 		if (byte_sent_header == -1)
 		{
 			this->_status = ERROR_WHILE_SENDING;
